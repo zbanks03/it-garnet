1e344eb8f212a549df7a339ccb1bb43ddd30127d
zbanks03 <145708971+zbanks03@users.noreply.github.com>
2024-04-24 17:09:15 +0000

    marsroverphoto

--- marsroverphoto/assets/script/marsrover.js
+++ marsroverphoto/assets/script/marsrover.js
@@ -3,55 +3,24 @@ function calculate() {
     /* Make sure that the form is valid */
     if ($( "#myform" ).valid()) {
         
-        /* get the operands from the form */
-        var FromValue = document.getElementById("FromValue").value;
-        
+        var apiKey = "F3BDMV4amDdKf1Ced9MnfENpYkpe480WeRD2fMLM"
+        var PictureDate = document.getElementById("PictureDate").value;
+
         /* figure out which from unit was checked and place the value in operator */
-        var fromunit;
-        if (document.getElementById("fromcentimeter").checked) {
-            fromunit = document.getElementById("fromcentimeter").value;
+        var myURL1 = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + Rover + "/photos?earth_date=" + PictureDate + "&api_key=" + apiKey;
+       
+        var Rover;
+        if (document.getElementById("curiosity").checked) {
+            Rover = document.getElementById("curiosity").value;
         }
-        if (document.getElementById("frommeter").checked) {
-            fromunit = document.getElementById("frommeter").value;
+        if (document.getElementById("opportunity").checked) {
+            Rover = document.getElementById("opportunity").value;
         }
-        if (document.getElementById("fromkilometer").checked) {
-            fromunit = document.getElementById("fromkilometer").value;
-        }
-        if (document.getElementById("frominch").checked) {
-            fromunit = document.getElementById("frominch").value;
-        }
-        if (document.getElementById("fromfeet").checked) {
-            fromunit = document.getElementById("fromfeet").value;
-        }
-        if (document.getElementById("fromyard").checked) {
-            fromunit = document.getElementById("fromyard").value;
-        }
-        if (document.getElementById("frommile").checked) {
-            fromunit = document.getElementById("frommile").value;
+        if (document.getElementById("spirit").checked) {
+            Rover = document.getElementById("spirit").value;
         }
 
-        var tounit;
-        if (document.getElementById("tocentimeter").checked) {
-            tounit = document.getElementById("tocentimeter").value;
-        }
-        if (document.getElementById("tometer").checked) {
-            tounit = document.getElementById("tometer").value;
-        }
-        if (document.getElementById("tokilometer").checked) {
-            tounit = document.getElementById("tokilometer").value;
-        }
-        if (document.getElementById("toinch").checked) {
-            tounit = document.getElementById("toinch").value;
-        }
-        if (document.getElementById("tofeet").checked) {
-            tounit = document.getElementById("tofeet").value;
-        }
-        if (document.getElementById("toyard").checked) {
-            tounit = document.getElementById("toyard").value;
-        }
-        if (document.getElementById("tomile").checked) {
-            tounit = document.getElementById("tomile").value;
-        }
+        
 
         CalculateResult(FromValue, fromunit, tounit)
 
@@ -59,42 +28,17 @@ function calculate() {
     }
 }
 
-async function CalculateResult(FromValue, fromunit, tounit) {
-        
-    // URL and method used with AJAX Call
-    var myURL = "https://brucebauer.info/assets/ITEC3650/unitsconversion.php";
 
-    /* AJAX calculator requires Operand1, Operator, and Operand2 */
-    myURL = myURL + "?FromValue=" + encodeURIComponent(FromValue) + "&FromUnit=" + encodeURIComponent(fromunit) + "&ToUnit=" + encodeURIComponent(tounit);
-
-    /* fetch the results */
-    let myCalcObject = await fetch(myURL);
-    let myResult = await myCalcObject.text();
-    
-    document.getElementById("ToValue").innerHTML = myResult.toString();
-}
 
 function clearform() {
     
-    document.getElementById("FromValue").value = "";
-    document.getElementById("FromValueMsg").innerHTML = "";
-    document.getElementById("fromcentimeter").checked = false;
-    document.getElementById("frommeter").checked = false;
-    document.getElementById("fromkilometer").checked = false;
-    document.getElementById("frominch").checked = false;
-    document.getElementById("fromfeet").checked = false;
-    document.getElementById("fromyard").checked = false;
-    document.getElementById("frommile").checked = false;
-    document.getElementById("tocentimeter").checked = false;
-    document.getElementById("tometer").checked = false;
-    document.getElementById("tokilometer").checked = false;
-    document.getElementById("toinch").checked = false;
-    document.getElementById("tofeet").checked = false;
-    document.getElementById("toyard").checked = false;
-    document.getElementById("tomile").checked = false;
-    document.getElementById("ToUnitMsg").innerHTML = "";
-    document.getElementById("FromUnitMsg").innerHTML = "";
-    document.getElementById("ToValue").innerHTML = "";
+    document.getElementById("Rover").value = "";
+    document.getElementById("RoverMsg").innerHTML = "";
+    document.getElementById("curiosity").checked = false;
+    document.getElementById("opportunity").checked = false;
+    document.getElementById("spirit").checked = false;
+    document.getElementById("PictureDateMsg").innerHTML = "";
+    document.getElementById("PictureDate").innerHTML = "";
 }
 
 /* Form Validation */
